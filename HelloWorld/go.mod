a3c35d5b144e71b30d41293bd0735f8d63f56c0c
tssud <tssudhish@gmail.com>
2020-12-12 15:27:52 +0000

    greetings setup - uses "Gladys"

--- HelloWorld/go.mod
+++ HelloWorld/go.mod
@@ -1,5 +1,7 @@
-module hello
+module hello.go
 
 go 1.15
 
-require rsc.io/quote v1.5.2 // indirect
+replace example.com/greetings => ../greetings
+
+require example.com/greetings v0.0.0-00010101000000-000000000000
--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
